388995c758a29538ee11635316a2f5c29a045527
Amar <Amar@test.com>
2020-05-02 16:53:31 +0530

    3rd commit

--- SeleniumAutomation/src/test/java/framework/TestClass1.java
+++ SeleniumAutomation/src/test/java/framework/TestClass1.java
@@ -17,11 +17,14 @@ public class TestClass1 {
   public void loginIntoAmazon() {
 	  boolean pageOpened = driver.getTitle().contains("Online Shopping site in India");
 	  Assert.assertEquals(pageOpened, true);
+	  driver.close();
   }
   @BeforeSuite
   public void beforeSuite() {
 	  //doing the setup
-	  System.setProperty("webdriver.chrome.driver", "C:\\Users\\amard\\git\\java\\SeleniumAutomation\\src\\test\\resources\\chromedriver.exe");
+	  String currentDirectory = System.getProperty("user.dir");
+	  System.out.println(currentDirectory+"\\src\\test\\resources\\\\src\\test\\resources");
+	  System.setProperty("webdriver.chrome.driver",currentDirectory + "\\src\\test\\resources\\chromedriver.exe");
 	  driver = new ChromeDriver();
 	  driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
 	  driver.get("https://www.amazon.in/");
